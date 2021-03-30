d5a755eb189dc0405a07ec5de6bcec54103b8836
Suman-cognologix <79828404+Suman-cognologix@users.noreply.github.com>
2021-03-30 11:38:30 +0530

    Update UnionLogProblem.scala

--- apache-spark/src/main/scala/com/cognologix/scala/spark/UnionLogProblem.scala
+++ apache-spark/src/main/scala/com/cognologix/scala/spark/UnionLogProblem.scala
@@ -12,5 +12,20 @@ package com.cognologix.scala.spark
  * Make sure the head lines are removed in the resulting RDD.
  */
 object UnionLogProblem {
+   def main(args: Array[String]): Unit = {
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val sc = new SparkContext("local[*]", "HelloWorld")
+
+    val july_log = sc.textFile("in/nasa_19950701.tsv")
+    val august_log = sc.textFile("in/nasa_19950801.tsv")
+    val result = july_log.union(august_log)
+    val result1 = result.filter( line => !(line.startsWith("host") && line.contains("bytes")))
+    val result2 = result1.sample(withReplacement = true, fraction = 0.1)
+
+    result2.saveAsTextFile("out/sample_nasa_logs.csv")
+    sc.stop()
+  }
 
 }
